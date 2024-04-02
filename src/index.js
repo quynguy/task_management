a03ddfbb4af8e47fdcaa32d941d717bf56cec79c
wquynguy <wquyenle@gmail.com>
2024-04-02 16:59:05 -0500

    added routes: dashboard, tasks, calendar, pprojects

--- src/index.js
+++ src/index.js
@@ -13,6 +13,22 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+app.get('/tasks', (req, res) => {
+    res.render('tasks');
+})
+
+app.get('/calendar', (req, res) => {
+    res.render('calendar');
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
